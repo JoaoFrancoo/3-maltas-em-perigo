2170e2a48070dcee964a8e7e8499bd9f1affe7b4
RunnMaatrox <joaofranco741@gmail.com>
2024-04-12 10:28:03 +0100

    bonito :D

--- app/src/main/java/com/example/a3_maltas_em_perigo_n1/MainActivity.kt
+++ app/src/main/java/com/example/a3_maltas_em_perigo_n1/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.a3_maltas_em_perigo_n1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +16,40 @@ class MainActivity : AppCompatActivity() {
 
         // Inicialize o Firebase Authentication
         auth = FirebaseAuth.getInstance()
+
+        // Inicialize o Firestore
+        db = FirebaseFirestore.getInstance()
+
+        // Adicionar um documento à coleção "users"
+        val user = hashMapOf(
+            "first" to "Ada",
+            "last" to "Lovelace",
+            "born" to 1815
+        )
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+
+                // Após adicionar o documento, recuperar todos os documentos da coleção "users"
+                db.collection("users")
+                    .get()
+                    .addOnSuccessListener { result ->
+                        for (document in result) {
+                            Log.d(TAG, "${document.id} => ${document.data}")
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.w(TAG, "Error getting documents.", exception)
+                    }
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
